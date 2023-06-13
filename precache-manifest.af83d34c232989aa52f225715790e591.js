1123719396bb2900122d827e1b635c6bbd4c7545
rdelena <rodnydelena@gmail.com>
2023-06-12 22:34:02 -0500

    Updates

---
+++ precache-manifest.af83d34c232989aa52f225715790e591.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1924374bf1284585e534999d4abc76e",
+    "url": "/411_wk5_day1_mapstatetoprops/index.html"
+  },
+  {
+    "revision": "5dafe4fcccf3daf470b2",
+    "url": "/411_wk5_day1_mapstatetoprops/static/css/main.05ad2c06.chunk.css"
+  },
+  {
+    "revision": "f2784e31bb806ea88f4e",
+    "url": "/411_wk5_day1_mapstatetoprops/static/js/2.ea1db622.chunk.js"
+  },
+  {
+    "revision": "5dafe4fcccf3daf470b2",
+    "url": "/411_wk5_day1_mapstatetoprops/static/js/main.86aa70e3.chunk.js"
+  },
+  {
+    "revision": "545c60bd1dc4d80ec1b9",
+    "url": "/411_wk5_day1_mapstatetoprops/static/js/runtime~main.50d41d53.js"
+  }
+]);
